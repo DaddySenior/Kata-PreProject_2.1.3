1226edcbb292f148006136eb87a456053518a402
DaddySenior <vinny2005@mail.ru>
2024-07-31 10:19:46 +0300

    class cat added

---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+    private String meow;
+
+    public String getMeow() {
+        return "MeOOOw";
+    }
+
+    public void setMeow(String message) {
+        this.meow = message;
+    }
+
+}
+
